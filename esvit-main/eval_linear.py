9f7e1c3b8e555dbe0641e8ba48c0718baeeed88b
Petteri Teikari <petteri@retispec.com>
2021-11-19 09:33:21 -0500

    top5 -> top2

--- esvit-main/eval_linear.py
+++ esvit-main/eval_linear.py
@@ -293,14 +293,16 @@ def validate_network(val_loader, model, linear_classifier, n, avgpool, depths):
         output = linear_classifier(output)
         loss = nn.CrossEntropyLoss()(output, target)
 
-        acc1, acc5 = utils.accuracy(output, target, topk=(1, 5))
+        k = 2 # placeholder in practice, as we have a binary classification with AD
+              # your top2 accuracy is always now 100%, but keep it here if you start having multiple classes
+        acc1, acc2 = utils.accuracy(output, target, topk=(1, k))
 
         batch_size = inp.shape[0]
         metric_logger.update(loss=loss.item())
         metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
-        metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
-    print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f} loss {losses.global_avg:.3f}'
-          .format(top1=metric_logger.acc1, top5=metric_logger.acc5, losses=metric_logger.loss))
+        metric_logger.meters['acc2'].update(acc2.item(), n=batch_size)
+    print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top2.global_avg:.3f} loss {losses.global_avg:.3f}'
+          .format(top1=metric_logger.acc1, top2=metric_logger.acc2, losses=metric_logger.loss))
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
 
